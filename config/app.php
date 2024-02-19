098dedd06906ee645611c76973031f93fbd66e8f
Hardip Patel <hardip@anormaly.com>
2024-02-19 16:52:02 +0530

    JJ-01 Add Version

--- config/app.php
+++ config/app.php
@@ -18,6 +18,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'version' => '0.0.1-alpha',
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment

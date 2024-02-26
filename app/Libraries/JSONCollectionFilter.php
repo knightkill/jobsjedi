2aabfb63743e0aaa89d28a8ee9a463d54e25d23a
Hardip Patel <hardip@anormaly.com>
2024-02-27 00:23:38 +0530

    JJ-32 #in-progress Add support for wildcard * and multiple columns (currently only for like and in_array)

--- app/Libraries/JSONCollectionFilter.php
+++ app/Libraries/JSONCollectionFilter.php
@@ -33,10 +33,10 @@ class JSONCollectionFilter
         $listing = collect($listing);
         switch($condition->operator){
             case 'in_array':
-                return in_array($condition->value, collect($listing)[$condition->field]);
+                return $this->evaluateInArray($condition, $listing);
                 break;
             case 'like':
-                return $this->recursiveContains($listing, $condition->value, $condition->field);
+                return $this->evaluateLike($condition, $listing);
                 break;
             case 'or' :
                 return $this->evaluateOr($listing, $condition->value);
@@ -46,6 +46,51 @@ class JSONCollectionFilter
                 $listing = collect([$listing]);
                 return $listing->contains($condition->field, $condition->operator, $condition->value);
         }
+    }
+
+    private function evaluateLike($condition, Collection $listing): bool
+    {
+        if ($condition->field === '*') {
+            $fields = $listing->flatMap(fn ($val) => $val)->keys()->toArray();
+            return collect($fields)->reduce(function($carry, $field) use ($listing, $condition) {
+                return $carry || $this->recursiveContains($listing, $condition->value, $field);
+            }, false);
+        }
+
+        if(Str::contains($condition->field, ',')) {
+            $fields = explode(',', $condition->field);
+            return collect($fields)->reduce(function($carry, $field) use ($listing, $condition) {
+                return $carry || $this->recursiveContains($listing, $condition->value, $field);
+            }, false);
+        }
+
+        return $this->recursiveContains($listing, $condition->value, $condition->field);
+    }
+
+    /**
+     * @param $condition
+     * @param Collection $listing
+     * @return bool
+     */
+    protected function evaluateInArray($condition, Collection $listing): bool
+    {
+        //If apply on all the fields
+        if($condition->field === '*') {
+            $fields = $listing->flatMap(fn ($val) => $val)->keys()->toArray();
+            return collect($fields)->reduce(function($carry, $field) use ($listing, $condition) {
+                return $carry || in_array($condition->value, collect($listing)[$field]);
+            }, false);
+        }
+
+        //If apply on multiple fields
+        if(Str::contains($condition->field, ',')) {
+            $fields = explode(',', $condition->field);
+            return collect($fields)->reduce(function($carry, $field) use ($listing, $condition) {
+                return $carry || in_array($condition->value, collect($listing)[$field]);
+            }, false);
+        }
+
+        return in_array($condition->value, collect($listing)[$condition->field]);
     }
 
     private function evaluateOr(Collection|array $listing, array $conditions): bool
@@ -71,14 +116,12 @@ class JSONCollectionFilter
     private function recursiveContains($haystack, $needle, $key): bool
     {
         foreach ($haystack as $index=>$item) {
-            if ($index === $key) {
-                if (Str::contains($item, Str::lower($needle), true)) {
-                    return true;
-                }
-            }
-
             if (is_array($item) || $item instanceof \Illuminate\Support\Collection) {
                 if ($this->recursiveContains($item, $needle, $key)) {
+                    return true;
+                }
+            }else if ($index === $key) {
+                if (Str::contains($item, Str::lower($needle), true)) {
                     return true;
                 }
             }
